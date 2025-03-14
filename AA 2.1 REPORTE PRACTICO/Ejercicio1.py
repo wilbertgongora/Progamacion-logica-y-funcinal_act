cfbe6f2f027d3d565be46aa1dbd2cfa158c1a2e9
wilbertgongora <211k0039@itscarrillopuerto.edu.mx>
2025-03-13 22:50:11 -0500

    Update Ejercicio1.py

---
+++ AA 2.1 REPORTE PRACTICO/Ejercicio1.py
@@ -0,0 +1,12 @@
+# Solicitar datos al usuario
+nombre_completo = input("Por favor, ingresa tu nombre completo: ")
+semestre = input("Ingresa tu semestre: ")
+grupo = input("Ingresa tu grupo: ")
+carrera = input("Ingresa tu carrera: ")
+
+# Mostrar los datos en pantalla
+print("\nDatos ingresados:")
+print(f"Nombre completo: {nombre_completo}")
+print(f"Semestre: {semestre}")
+print(f"Grupo: {grupo}")
+print(f"Carrera: {carrera}")
